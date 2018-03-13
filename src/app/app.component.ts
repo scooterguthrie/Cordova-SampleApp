bc566d7e8bfa2e65862ae42f0c1a36d93c9f4e96
Scott Guthrie <jsguthrie@nc.rr.com>
2018-03-13 16:15:50 -0400

    Build and run Android Emulator with script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var device;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    document.addEventListener('deviceready', function () {
+      alert(device.platform);
+    }, false);
+  }
+
 }
