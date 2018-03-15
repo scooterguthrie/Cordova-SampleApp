62374417b6cc509e79427b0a0c942e0c5e7a8272
Scott Guthrie <jsguthrie@nc.rr.com>
2018-03-15 10:47:59 -0400

    modify android build script

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-// Enable production mode unless running locally
-if (!/localhost/.test(document.location.host)) {
-  enableProdMode();
-}
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
